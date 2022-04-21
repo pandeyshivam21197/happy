a78636f0264a6669072d9b2279b4bde95e576cd9
Shivam <Shivam@Ones-MacBook-Pro.local>
2022-04-22 00:22:51 +0530

    Shivam: added all screens to lang localization

--- packages/mobile/src/navigation/appNavigator/index.tsx
+++ packages/mobile/src/navigation/appNavigator/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {BottomTabNavigator} from '@happy/mobile/src/navigation/appNavigator/bottomTabNavigator';
 import {NavigationKeys} from '@happy/mobile/src/navigation/constants';
-import {SignUpScreen} from '../../screens/auth/SignUpScreen';
+import {SignUpScreen} from '../../screens';
 
 const AppStack = createNativeStackNavigator();
 
