ebb0daf09bca17c3c3d8611951dd1b99ae7e3361
pandey21197 <shivam@onecare.co.in>
2022-05-09 00:56:49 +0530

    Shivam: refactored

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -9,14 +9,17 @@
 import React, {FC} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import RootNavigator from '@happy/mobile/src/navigation';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 // import '@happy/common/src/services/locale/MultiLingualService';
 
 const App = (): any => {
   return (
-    <SafeAreaView style={styles.appContainer}>
-      <StatusBar barStyle={'dark-content'} />
-      <RootNavigator />
-    </SafeAreaView>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <SafeAreaView style={styles.appContainer}>
+        <StatusBar barStyle={'dark-content'} />
+        <RootNavigator />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
