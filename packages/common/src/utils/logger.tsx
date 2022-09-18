ffbf241b4ac6ede3f3e7a62c51fcd89fe0e1e5e3
pandey21197 <shivam@onecare.co.in>
2022-09-19 01:13:00 +0530

    Shivam: updated logger

--- packages/common/src/utils/logger.tsx
+++ packages/common/src/utils/logger.tsx
@@ -1,9 +1,9 @@
 enum LogLevel {
-  Trace = 'trace',
-  Debug = 'debug',
-  Info = 'info',
-  Warn = 'warn',
-  Error = 'error',
+  Trace = "trace",
+  Debug = "debug",
+  Info = "info",
+  Warn = "warn",
+  Error = "error",
 }
 
 export default class Logger {
@@ -17,32 +17,36 @@ export default class Logger {
     this._isDebugMode = isDebug;
   }
 
-  public trace = (message: string): void => {
-    this.logWithLevel(LogLevel.Trace, message);
-  }
+  public trace = (message: any, ...rest: any[]): void => {
+    this.logWithLevel(LogLevel.Trace, message, ...rest);
+  };
 
-  public debug = (message: string): void => {
-    this.logWithLevel(LogLevel.Debug, message);
-  }
+  public debug = (message: any, ...rest: any[]): void => {
+    this.logWithLevel(LogLevel.Debug, message, ...rest);
+  };
 
-  public info = (message: string): void => {
-    this.logWithLevel(LogLevel.Info, message);
-  }
+  public info = (message: any, ...rest: any[]): void => {
+    this.logWithLevel(LogLevel.Info, message, ...rest);
+  };
 
-  public warn = (message: string): void => {
-    this.logWithLevel(LogLevel.Warn, message);
-  }
+  public warn = (message: any, ...rest: any[]): void => {
+    this.logWithLevel(LogLevel.Warn, message, ...rest);
+  };
 
-  public error = (message: string): void => {
-    this.logWithLevel(LogLevel.Error, message);
-  }
+  public error = (message: any, ...rest: any[]): void => {
+    this.logWithLevel(LogLevel.Error, message, ...rest);
+  };
 
-  private logWithLevel = (logLevel: LogLevel, message: any): void => {
-    const logMessage = JSON.parse(JSON.stringify(message));
+  private logWithLevel = (
+    logLevel: LogLevel,
+    message: any,
+    ...rest: any[]
+  ): void => {
+    const logMessage = message ? JSON.parse(JSON.stringify(message)) : message;
     if (!this.isDebugMode) {
-      console[logLevel.toString() as LogLevel](logMessage);
+      console[logLevel.toString() as LogLevel](logMessage, ...rest);
     }
-  }
+  };
 }
 
 const logger = new Logger();
